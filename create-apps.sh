a7b7165aa4b7fd501f10dac932c3cc509e9f2cd9
Martyn Janes <martyn@obany.com>
2017-12-01 09:56:07 +0300

    Added moment package

--- create-apps.sh
+++ create-apps.sh
@@ -2,11 +2,11 @@
 set -e
 
 node unitejs/cli/bin/unite configure --packageName=vanilla-javascript --title="Vanilla JavaScript" --profile=VanillaJavaScript --outputDirectory=./apps/vanilla-javascript
-# node unitejs/cli/bin/unite package --packageName=moment --outputDirectory=./apps/vanilla-javascript
+node unitejs/cli/bin/unite package --packageName=moment --outputDirectory=./apps/vanilla-javascript
 
 node unitejs/cli/bin/unite configure --packageName=vanilla-typescript-commonjs --title="Vanilla TypeScript CommonJS" --profile=VanillaTypeScript --moduleType=CommonJS --bundler=Webpack --outputDirectory=./apps/vanilla-typescript-commonjs
-# node unitejs/cli/bin/unite package --packageName=moment --outputDirectory=./apps/vanilla-typescript-commonjs
+node unitejs/cli/bin/unite package --packageName=moment --outputDirectory=./apps/vanilla-typescript-commonjs
 
 node unitejs/cli/bin/unite configure --packageName=vanilla-typescript-systemjs --title="Vanilla TypeScript SystemJS" --profile=VanillaTypeScript --moduleType=SystemJS --bundler=SystemJSBuilder --outputDirectory=./apps/vanilla-typescript-systemjs
-# node unitejs/cli/bin/unite package --packageName=moment --outputDirectory=./apps/vanilla-typescript-systemjs
+node unitejs/cli/bin/unite package --packageName=moment --outputDirectory=./apps/vanilla-typescript-systemjs
 
