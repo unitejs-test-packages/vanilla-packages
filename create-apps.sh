389c5cadadfdc5299e9315b6b4c2f7448cfa78f0
Martyn Janes <martyn@obany.com>
2017-12-11 14:20:49 +0300

    Updated module paths

--- create-apps.sh
+++ create-apps.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-node unitejs/cli/bin/unite configure --packageName=vanilla-javascript --title="Vanilla JavaScript" --profile=VanillaJavaScript --outputDirectory=./apps/vanilla-javascript
-node unitejs/cli/bin/unite package --packageName=moment --outputDirectory=./apps/vanilla-javascript
+node unitejs/unitejs-cli/bin/unite configure --packageName=vanilla-javascript --title="Vanilla JavaScript" --profile=VanillaJavaScript --outputDirectory=./apps/vanilla-javascript
+node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/vanilla-javascript
 
-node unitejs/cli/bin/unite configure --packageName=vanilla-typescript-commonjs --title="Vanilla TypeScript CommonJS" --profile=VanillaTypeScript --moduleType=CommonJS --bundler=Webpack --outputDirectory=./apps/vanilla-typescript-commonjs
-node unitejs/cli/bin/unite package --packageName=moment --outputDirectory=./apps/vanilla-typescript-commonjs
+node unitejs/unitejs-cli/bin/unite configure --packageName=vanilla-typescript-commonjs --title="Vanilla TypeScript CommonJS" --profile=VanillaTypeScript --moduleType=CommonJS --bundler=Webpack --outputDirectory=./apps/vanilla-typescript-commonjs
+node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/vanilla-typescript-commonjs
 
-node unitejs/cli/bin/unite configure --packageName=vanilla-typescript-systemjs --title="Vanilla TypeScript SystemJS" --profile=VanillaTypeScript --moduleType=SystemJS --bundler=SystemJSBuilder --outputDirectory=./apps/vanilla-typescript-systemjs
-node unitejs/cli/bin/unite package --packageName=moment --outputDirectory=./apps/vanilla-typescript-systemjs
+node unitejs/unitejs-cli/bin/unite configure --packageName=vanilla-typescript-systemjs --title="Vanilla TypeScript SystemJS" --profile=VanillaTypeScript --moduleType=SystemJS --bundler=SystemJSBuilder --outputDirectory=./apps/vanilla-typescript-systemjs
+node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/vanilla-typescript-systemjs
 
