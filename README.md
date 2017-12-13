7b19781797f9297054ec3d9ba70df7afd4497189
Martyn Janes <martyn@obany.com>
2017-12-13 10:13:27 +0300

    Added lodash

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## UniteJS Operations
 
 * unite configure --profile=VanillaJavaScript/VanillaTypeScript
+* unite package --packageName=lodash
 * unite package --packageName=moment
 
 ## Build Operations
--- create-apps.sh
+++ create-apps.sh
@@ -2,11 +2,13 @@
 set -e
 
 node unitejs/unitejs-cli/bin/unite configure --packageName=vanilla-javascript --title="Vanilla JavaScript" --profile=VanillaJavaScript --outputDirectory=./apps/vanilla-javascript
+node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/vanilla-javascript
 node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/vanilla-javascript
 
 node unitejs/unitejs-cli/bin/unite configure --packageName=vanilla-typescript-commonjs --title="Vanilla TypeScript CommonJS" --profile=VanillaTypeScript --moduleType=CommonJS --bundler=Webpack --outputDirectory=./apps/vanilla-typescript-commonjs
+node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/vanilla-typescript-commonjs
 node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/vanilla-typescript-commonjs
 
 node unitejs/unitejs-cli/bin/unite configure --packageName=vanilla-typescript-systemjs --title="Vanilla TypeScript SystemJS" --profile=VanillaTypeScript --moduleType=SystemJS --bundler=SystemJSBuilder --outputDirectory=./apps/vanilla-typescript-systemjs
+node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/vanilla-typescript-systemjs
 node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/vanilla-typescript-systemjs
-
