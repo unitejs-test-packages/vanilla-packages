4b8ac95263e5b0a199dd9df0c0c947777230ab22
Martyn Janes <martyn@obany.com>
2017-12-18 10:27:45 +0300

    Added JavaScript SystemJS Builds

--- create-apps.sh
+++ create-apps.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 set -e
 
-node unitejs/unitejs-cli/bin/unite configure --packageName=vanilla-javascript --title="Vanilla JavaScript" --profile=VanillaJavaScript --outputDirectory=./apps/vanilla-javascript
-mkdir -p ./apps/vanilla-javascript/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.js $_
-mkdir -p ./apps/vanilla-javascript/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers/*.js $_
-node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/vanilla-javascript
-node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/vanilla-javascript
-node unitejs/unitejs-cli/bin/unite package --packageName=rxjs --outputDirectory=./apps/vanilla-javascript
-node unitejs/unitejs-cli/bin/unite package --packageName=underscore --outputDirectory=./apps/vanilla-javascript
+node unitejs/unitejs-cli/bin/unite configure --packageName=vanilla-javascript-commonjs --title="Vanilla JavaScript CommonJS" --profile=VanillaJavaScript --moduleType=CommonJS --bundler=Webpack --outputDirectory=./apps/vanilla-javascript-commonjs
+mkdir -p ./apps/vanilla-javascript-commonjs/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.js $_
+mkdir -p ./apps/vanilla-javascript-commonjs/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers/*.js $_
+node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/vanilla-javascript-commonjs
+node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/vanilla-javascript-commonjs
+node unitejs/unitejs-cli/bin/unite package --packageName=rxjs --outputDirectory=./apps/vanilla-javascript-commonjs
+node unitejs/unitejs-cli/bin/unite package --packageName=underscore --outputDirectory=./apps/vanilla-javascript-commonjs
+
+node unitejs/unitejs-cli/bin/unite configure --packageName=vanilla-javascript-systemjs --title="Vanilla JavaScript SystemJS" --profile=VanillaJavaScript --moduleType=SystemJS --bundler=SystemJSBuilder --outputDirectory=./apps/vanilla-javascript-systemjs
+mkdir -p ./apps/vanilla-javascript-systemjs/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.js $_
+mkdir -p ./apps/vanilla-javascript-systemjs/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers/*.js $_
+node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/vanilla-javascript-systemjs
+node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/vanilla-javascript-systemjs
+node unitejs/unitejs-cli/bin/unite package --packageName=rxjs --outputDirectory=./apps/vanilla-javascript-systemjs
+node unitejs/unitejs-cli/bin/unite package --packageName=underscore --outputDirectory=./apps/vanilla-javascript-systemjs
 
 node unitejs/unitejs-cli/bin/unite configure --packageName=vanilla-typescript-commonjs --title="Vanilla TypeScript CommonJS" --profile=VanillaTypeScript --moduleType=CommonJS --bundler=Webpack --outputDirectory=./apps/vanilla-typescript-commonjs
 mkdir -p ./apps/vanilla-typescript-commonjs/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.ts $_
